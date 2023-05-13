aef629f84f07616911df653c842f23f42e8020d8
Sahil Josan <106590141+Sahiljosan@users.noreply.github.com>
2023-05-14 01:04:24 +0530

    Update application.py

--- application.py
+++ application.py
@@ -5,9 +5,9 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 
-application = app = Flask(__name__)
+application = Flask(__name__)
 
-# app = application
+app = application
 
 ## Route for a home page
 
